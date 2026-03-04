c7f835f114b9f6b290742f9b2c4488bdd199ddb6
Adam <adammarvin05@gmail.com>
2026-03-04 01:41:16 +0000

    ^^

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -25,28 +25,6 @@ public class PlayerMove : MonoBehaviour
         input.x = Input.GetAxis("Horizontal");
         input.y = Input.GetAxis("Vertical");
 
-        // NEW: Build movement direction relative to camera
-        Vector3 camForward = cam.transform.forward;
-        Vector3 camRight   = cam.transform.right;
-
-        // Flatten to horizontal plane so vertical camera tilt doesn't affect movement
-        camForward.y = 0f;
-        camRight.y   = 0f;
-        camForward.Normalize();
-        camRight.Normalize();
-
-        Vector3 moveDir = (camForward * input.y + camRight * input.x);
-
-        Vector3 targetVelocity = moveDir.normalized * speed * moveDir.magnitude;
-        velocity = Vector3.MoveTowards(velocity, targetVelocity, acceleration * Time.deltaTime);
-
-        // NEW: Rotate player to face movement direction
-        if (moveDir.sqrMagnitude > 0.01f)
-        {
-            Quaternion targetRotation = Quaternion.LookRotation(moveDir);
-            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, turnSpeed * Time.deltaTime);
-        }
-
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
@@ -56,7 +34,26 @@ public class PlayerMove : MonoBehaviour
 
     void FixedUpdate()
     {
+        Vector3 camForward = cam.transform.forward;
+        Vector3 camRight   = cam.transform.right;
+        camForward.y = 0f;
+        camRight.y   = 0f;
+        camForward.Normalize();
+        camRight.Normalize();
+
+        Vector3 moveDir = (camForward * input.y + camRight * input.x);
+        Vector3 targetVelocity = moveDir.normalized * speed * moveDir.magnitude;
+
+        velocity = Vector3.MoveTowards(velocity, targetVelocity, acceleration * Time.fixedDeltaTime);
         rb.velocity = new Vector3(velocity.x, rb.velocity.y, velocity.z);
+
+        if (moveDir.sqrMagnitude > 0.01f)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(moveDir);
+            transform.rotation = Quaternion.Slerp(
+                transform.rotation, targetRotation, turnSpeed * Time.fixedDeltaTime
+            );
+        }
     }
 
     void OnCollisionEnter(Collision other)
